a41aef9a9ebffbadae48e99a935c2e184ef7c0a1
Marcus Wyatt <marcus.wyatt@visfleet.com>
2010-02-24 23:31:54 +1300

    Can't require the rake tasks in the main lib/swf_fu file.

--- lib/swf_fu.rb
+++ lib/swf_fu.rb
@@ -1,3 +1,2 @@
-require File.join(File.dirname(__FILE__), '/swf_fu/tasks')
 require File.join(File.dirname(__FILE__), '/action_view/helpers/swf_fu_helper')
 require File.join(File.dirname(__FILE__), '/action_view/helpers/asset_tag_helper/swf_asset')
