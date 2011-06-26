cfe35955ec2260e9ddcbac3e93bfae2c0161b37e
Rodrigo Benenson <rodrigo.benenson@gmail.com>
2011-06-27 00:06:46 +0200

    Added the Rails 3.1 engine that allows the rails application to access the gem assets

--- lib/swf_fu.rb
+++ lib/swf_fu.rb
@@ -1,2 +1,14 @@
 require File.join(File.dirname(__FILE__), '/action_view/helpers/swf_fu_helper')
 require File.join(File.dirname(__FILE__), '/action_view/helpers/asset_tag_helper/swf_asset')
+
+module SwfFu
+  module Rails
+      
+      
+          if ::Rails.version >= "3.1"
+	        require 'swf_fu/rails/engine'
+	  end
+	
+				    end
+				    end
+				    
---
+++ lib/swf_fu/rails/engine.rb
@@ -0,0 +1,13 @@
+# Configure Rails 3.1 
+module SwfFu
+  module Rails
+  
+      class Engine < ::Rails::Engine
+            #config.before_configuration do
+	    #require "jquery/assert_select" if ::Rails.env.test?
+	    #end
+			      end
+			      
+			        end
+			       end
+				
